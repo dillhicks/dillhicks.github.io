2d9679147cbe313573e89da190c578b8c33c4185
dillhicks <hicksdillon56@gmail.com>
2025-04-10 01:34:57 -0700

    Remove exportPathMap and update Next.js configuration for App Router

--- next.config.js
+++ next.config.js
@@ -6,12 +6,6 @@ const nextConfig = {
   },
   trailingSlash: true,
   distDir: 'out',
-  exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-      '/404': { page: '/404' },
-    }
-  }
 }
 
 module.exports = nextConfig 
